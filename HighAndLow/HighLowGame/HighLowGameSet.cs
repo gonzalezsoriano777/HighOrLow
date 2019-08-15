d5a8175a1e119923d182ad7127069518feef0b56
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-14 22:15:11 -0500

    HighLow game setup ready to work on...

---
+++ HighAndLow/HighLowGame/HighLowGameSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighAndLow.HighLowGame
+{
+    public class HighLowGameSet
+    {
+        public void GameProcess()
+        {
+
+        }
+    }
+}
