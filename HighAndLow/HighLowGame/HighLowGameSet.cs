8a0610ba214b452067743e7b9684991e75e5891b
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-14 22:52:42 -0500

    Set up complete. 2.0 to 2.2 is set up with a layout.

--- HighAndLow/HighLowGame/HighLowGameSet.cs
+++ HighAndLow/HighLowGame/HighLowGameSet.cs
@@ -8,6 +8,9 @@ namespace HighAndLow.HighLowGame
 {
     public class HighLowGameSet : Card
     {
+
+        public int correctPick = 0;
+
         public void GameProcess()
         {
 
@@ -18,7 +21,7 @@ namespace HighAndLow.HighLowGame
 
             if(firstCard.value == secondCard.value && firstCard.suit == secondCard.suit)
             {
-                var displayNextCard = new Card(); 
+                var anotherCard = new Card(); 
             }
             else
             {
@@ -26,7 +29,27 @@ namespace HighAndLow.HighLowGame
                 Console.WriteLine("{0} {1}", firstCard.value, firstCard.suit);
                 Console.WriteLine("Now let's play high or low...");
 
+                string inputGuess = Console.ReadLine();
 
+                if (inputGuess.ToLower() != "high" || inputGuess.ToLower() != "low")
+                {
+                    Console.WriteLine("Please choose between high or low: ");
+                }
+                else if
+                    (secondCard.value > firstCard.value && inputGuess.ToLower() == "high" || secondCard.value < firstCard.value && inputGuess.ToLower() == "low")
+                {
+                    Console.WriteLine("Correct! You chose wisely.");
+                    correctPick++;
+                    secondCard = firstCard;
+                    secondCard = new Card();
+                    Console.WriteLine("{0}: Let's see how far you go with this streak of yours.", correctPick);
+                }
+                else if
+                    (secondCard.value < firstCard.value && inputGuess.ToLower() == "high" || secondCard.value > firstCard.value && inputGuess.ToLower() == "low")
+                {
+                    Console.WriteLine("Goodbye, better luck next time");
+                    correctPick = 0;
+                }
 
 
             }
