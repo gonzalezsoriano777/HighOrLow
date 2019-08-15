40659988fc7d81a63ee302a8d39180b6eeee5481
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-14 22:27:16 -0500

    Processing...

--- HighAndLow/HighLowGame/HighLowGameSet.cs
+++ HighAndLow/HighLowGame/HighLowGameSet.cs
@@ -11,6 +11,25 @@ namespace HighAndLow.HighLowGame
         public void GameProcess()
         {
 
+           // Console.WriteLine("This is a test..");
+
+            var firstCard = new Card();
+            var secondCard = new Card();
+
+            if(firstCard.value == secondCard.value && firstCard.suit == secondCard.suit)
+            {
+                var displayNextCard = new Card(); 
+            }
+            else
+            {
+                Console.WriteLine("Here's your first card.");
+                Console.WriteLine("{0} {1}", firstCard.value, firstCard.suit);
+                Console.WriteLine("Now let's play high or low...");
+
+
+
+
+            }
 
         }
     }
--- HighAndLow/Program.cs
+++ HighAndLow/Program.cs
@@ -13,13 +13,17 @@ namespace HighAndLow
     {
         static void Main(string[] args)
         {
-
+            /*
             Card aCard = new Card();
             Console.WriteLine("aCard : {0} of {1}", aCard.value, aCard.suitString());
         
             Card myCard = new Card(11, 2);
             Console.WriteLine("myCard : {0} of {1}", myCard.value, myCard.suitString());
-            
+            */
+
+            HighLowGameSet game = new HighLowGameSet();
+            game.GameProcess();
+
         }
     }
 }
