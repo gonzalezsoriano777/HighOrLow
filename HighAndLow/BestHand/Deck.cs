54f39da6a7fb8b6cb04229944bb79ca7a75c6a52
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-15 19:26:10 -0500

    Created a deck

---
+++ HighAndLow/BestHand/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighAndLow.BestHand
+{
+    public class Deck
+    {
+
+
+
+        // for the two people play for the best of hand
+        List<int> firstHandDeck = new List<int>();
+        List<int> secondHandDeck = new List<int>();
+
+
+
+    }
+}
